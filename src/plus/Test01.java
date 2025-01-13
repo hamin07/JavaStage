8233fc4daff4f777b3dc75c0f82156f74108b6ee
hrmin <haminyang2@naver.com>
2025-01-13 17:41:53 +0900

    Add files via upload

---
+++ src/plus/Test01.java
@@ -0,0 +1,10 @@
+package plus;
+
+import java.util.Scanner;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hello World");
+	}
+}
